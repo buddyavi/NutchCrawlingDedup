2e4551d73374f1a726188a06e03dfdd5897b39ca
Avi Sanadhya <asanadhy@usc.edu>
2015-02-23 01:15:20 -0800

    Commiting near duplicates algorithm

---
+++ RetrievalAssignment1/src/com/usc/crawler/dedup/NearDuplicates.java
@@ -0,0 +1,190 @@
+package com.usc.crawler.dedup;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import com.skjegstad.utils.BloomFilter;
+import com.usc.crawler.constatnts.Constants;
+import com.usc.crawler.model.Record;
+
+public class NearDuplicates {
+
+	static ArrayList<Record> allRecords = new ArrayList<Record>();
+	static Map<Record, ArrayList<Record>> similarRecordMap = new HashMap<Record, ArrayList<Record>>();
+
+	/**
+	 * This method takes the Record bean and populates a Map data structure
+	 * accordingly. Key for the map is recordId/MD5 digest and value is the
+	 * record object
+	 * 
+	 * @param rc
+	 */
+	public static void recordsToMap(Record rc) {
+
+		allRecords.add(rc);
+	}
+
+	public ArrayList<Record> getAllRecords() {
+		return allRecords;
+	}
+
+	public void setAllRecords(ArrayList<Record> allRecords) {
+		this.allRecords = allRecords;
+	}
+
+	/**
+	 * This method reads the file which contains the parsed text and metadata
+	 * and populates the record beans accordingly.
+	 * 
+	 * @param file
+	 */
+	public void fileToBean(String file) {
+		String line;
+		int recordId = 0;
+		String url = "";
+		String parseText = "";
+		String title = "";
+		String contentMetadata = "";
+		String parseMetadata = "";
+		String digestMD5 = "";
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while ((line = br.readLine()) != null) {
+				if (line.startsWith("Recno")) {
+					recordId = Integer.parseInt(line.split("::")[1].trim());
+				}
+				if (line.startsWith("URL")) {
+					url = line.split("::")[1].trim();
+				}
+				if (line.startsWith("ParseText")) {
+					parseText = line.split("::")[1].trim();
+				}
+				if (line.startsWith("Title")) {
+					title = line.split(":")[1].trim();
+				}
+				if (line.startsWith("Content Metadata")) {
+					contentMetadata = line.split(":")[1].trim();
+					digestMD5 = contentMetadata.split(" ")[1].split("=")[1]
+							.trim();
+				}
+				if (line.startsWith("Parse Metadata")) {
+					parseMetadata = line.split(":")[1].trim();
+				}
+				if (line.startsWith("#####")) {
+					Record rc = new Record(recordId, url, parseText, title,
+							contentMetadata, digestMD5, parseMetadata);
+					recordId = 0;
+					url = "";
+					parseText = "";
+					title = "";
+					contentMetadata = "";
+					parseMetadata = "";
+					digestMD5 = "";
+					recordsToMap(rc);
+				}
+			}
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Method that takes bloom filter for one document and compares it with all
+	 * other documents to calculate similarity
+	 * 
+	 * @param myBloom
+	 * @param id
+	 */
+	public static void compareToAllRecords(BloomFilter<String> myBloom,
+			Record parent) {
+		int containCount = 0;
+		int notFoundCount = 0;
+		// setting the threshold , incoming document should match atleast 90% of
+		// elements in current document
+		int threshold = (myBloom.count() * 90) / 100;
+
+		for (Record rc : allRecords) {
+			containCount = 0;
+			notFoundCount = 0;
+			ArrayList<Record> similarRc = new ArrayList<Record>();
+			if (rc.getRecordId() != parent.getRecordId()) {
+				String text = rc.getParseText();
+
+				StringTokenizer st = new StringTokenizer(text);
+
+				while (st.hasMoreElements()) {
+					if (myBloom.contains(st.nextToken())) {
+						containCount += 1;
+					} else {
+						notFoundCount += 1;
+					}
+				}
+				// To normalize comparison between uneven size of Documents,
+				// check both comparison with threshold
+				// and also comparison with notFound elements. notFoundCount > 0
+				// checks and removes exact duplicates
+				if ((containCount > notFoundCount)
+						&& (containCount > threshold) && (notFoundCount > 0)) {
+
+					similarRc.add(rc);
+				}
+				if (similarRc.size() > 0) {
+					similarRecordMap.put(parent, similarRc);
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * method to get all similar records and populate the hash map holding
+	 * parent record and similar records
+	 */
+	public void getSimilarityMatrix() {
+		double falsePositiveProbability = 0.1;
+		int expectedNumberOfElements = 1000;
+		for (Record rc : allRecords) {
+			BloomFilter<String> myBloom = new BloomFilter<String>(
+					falsePositiveProbability, expectedNumberOfElements);
+			String text = rc.getParseText();
+			StringTokenizer st = new StringTokenizer(text);
+			while (st.hasMoreElements()) {
+				myBloom.add(st.nextToken());
+			}
+			compareToAllRecords(myBloom, rc);
+
+		}
+
+	}
+
+	public static void main(String args[]) {
+		NearDuplicates ndp = new NearDuplicates();
+		ndp.fileToBean(Constants.INPUT_FILE_NAME);
+		ndp.getSimilarityMatrix();
+
+		Iterator it = similarRecordMap.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			System.out.println("Parent Record : "
+					+ ((Record) pair.getKey()).getRecordId());
+			ArrayList<Record> matchedRecords = (ArrayList<Record>) pair.getValue();
+			for (Record rc : matchedRecords) {
+				System.out.println("Similar Record : " + rc.getRecordId());
+			}
+
+			it.remove(); // avoids a ConcurrentModificationException
+			System.out.println("**************");
+		}
+	}
+
+}
